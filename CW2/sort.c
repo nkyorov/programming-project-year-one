dad13e80041f3bbcaab6be53fc3fd0f80805ca85
Nikolay Kyorov [sc16nsk] <sc16nsk@comp-pc3025.leeds.ac.uk>
2017-05-03 15:28:02 +0100

    Microseconds(prototype)

--- CW2/sort.c
+++ CW2/sort.c
@@ -20,7 +20,8 @@ double bubbleSort(int numbers[], int size){
   }
   end = clock();
   double bubbleTime=((double)(end-start))/ CLOCKS_PER_SEC;
-  printf("\nSort took %f seconds.\n",bubbleTime);
+  printf("\nSort took %f microseconds.\n",bubbleTime*1000000);
+  printf("\nSort took %ld microseconds.\n",bubbleTime*1000000);
   printf("Bubble sort done!\n");
   return bubbleTime;
 }
